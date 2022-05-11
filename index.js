a73df0b9e4f06a61eede0f20811f692a1f09a874
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-11 18:28:43 +0600

    Connected with database

---
+++ index.js
@@ -0,0 +1,37 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mycluster.rn7n6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+console.log(uri);
+
+async function run(){
+    try{
+        await client.connect();
+        const productsCollection = client.db("ProductDB").collection("products")
+
+        app.get('/products', async(req, res) => {
+            const result = await productsCollection.find({}).toArray()
+            res.send(result)
+        })
+    }
+    finally{}
+}
+
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
