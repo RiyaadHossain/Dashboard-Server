a556fc7aaf78425b364abefc39ce7e357955e516
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-11 22:48:20 +0600

    delete option added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -13,30 +13,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myclust
 const client = new MongoClient(uri);
 console.log(uri);
 
-async function run(){
-    try{
-        await client.connect();
-        const productsCollection = client.db("ProductDB").collection("products")
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("ProductDB").collection("products");
 
-        app.get('/products', async(req, res) => {
-            const result = await productsCollection.find({}).toArray()
-            res.send(result)
-        })
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find({}).toArray();
+      if (!result?.length) return res.send({ success: false, error: "No Data Found" });
+      res.send({ success: true, data: result });
+    });
 
-        app.post('/product', async(req, res)=> {
-          const product = req.body
-          console.log(product.name);
-          if(!product.name || !product.price || !product.url){
-            return res.send({success: false, error: 'Please Provide all Details'})
-          }
-          const result = await productsCollection.insertOne(product);
-          res.send({success: true, message: `You Successfully added ${product.name}`})
-        })
-    }
-    finally{}
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      console.log(product.name);
+      if (!product.name || !product.price || !product.imgUrl) {
+        return res.send({
+          success: false,
+          error: "Please Provide all Details",
+        });
+      }
+      const result = await productsCollection.insertOne(product);
+      res.send({
+        success: true,
+        message: `You Successfully added ${product.name}`,
+      });
+    });
+
+    app.delete('/product/:id', async(req, res) => {
+      const id = {_id: ObjectId(req.params.id)}
+      const result = await productsCollection.deleteOne(id)
+      res.send({success: true, message: `You have Deleted successfully`})
+    })
+  } finally {
+  }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
